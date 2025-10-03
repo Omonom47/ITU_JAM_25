cb7b2e020a9a538a61854368248f3f7a7de787b5
Maxime Havez <56996234+Omonom47@users.noreply.github.com>
2025-10-03 19:34:10 +0200

    Tower placement at mouse position

--- Assets/Scripts/TowerPlacement.cs
+++ Assets/Scripts/TowerPlacement.cs
@@ -1,13 +1,14 @@
 using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class TowerPlacement : MonoBehaviour, InputSystem_Actions.IPlayerActions
 {
-    [SerializeField] private GameObject _prefab;
+    [SerializeField] private GameObject towerPrefab;
     private InputSystem_Actions _inputSystemActions;
 
-    private Vector2 _position;
+    private Vector2 _mousePosition;
 
     private void OnEnable()
     {
@@ -15,45 +16,24 @@ public class TowerPlacement : MonoBehaviour, InputSystem_Actions.IPlayerActions
         _inputSystemActions.Player.SetCallbacks(this);
         _inputSystemActions.Player.Enable();
     }
-
+    
     private void OnDisable()
     {
         _inputSystemActions.Player.Disable();
-    }   
-
-    public void OnMove(InputAction.CallbackContext context)
-    {
     }
 
     public void OnLook(InputAction.CallbackContext context) => 
-        _position = context.action.ReadValue<Vector2>();
+        _mousePosition = context.action.ReadValue<Vector2>();
 
     public void OnAttack(InputAction.CallbackContext context)
     {
-        Instantiate(_prefab, _position, Quaternion.identity);
+        if (context.action.WasPressedThisFrame())
+        {
+            var mouseWorldPos = Camera.main.ScreenToWorldPoint(_mousePosition);
+            mouseWorldPos.z = 0;
+            Instantiate(towerPrefab, mouseWorldPos, Quaternion.identity);
+        }
+        
     }
 
-    public void OnInteract(InputAction.CallbackContext context)
-    {
-    }
-
-    public void OnCrouch(InputAction.CallbackContext context)
-    {
-    }
-
-    public void OnJump(InputAction.CallbackContext context)
-    {
-    }
-
-    public void OnPrevious(InputAction.CallbackContext context)
-    {
-    }
-
-    public void OnNext(InputAction.CallbackContext context)
-    {
-    }
-
-    public void OnSprint(InputAction.CallbackContext context)
-    {
-    }
 }
