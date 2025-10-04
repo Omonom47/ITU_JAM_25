dc54d284089caa8383ff00b8e0e61403589b8c37
Maxime Havez <56996234+Omonom47@users.noreply.github.com>
2025-10-04 15:57:33 +0200

    replaces map with version including middle wall

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -3,18 +3,18 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     [SerializeField] private GameObject _cursorObject;
-    
-    
+
+    private RectTransform _rectTransform;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _rectTransform = _cursorObject.GetComponent<RectTransform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _rectTransform.position += Vector3.one;
     }
 }
