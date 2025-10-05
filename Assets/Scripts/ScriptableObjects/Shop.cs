56df9269240185c411c2556fa7ab0b79fcdda4c2
William Skou Heidemann <williamskou2227@gmail.com>
2025-10-05 12:04:26 +0200

    shop

---
+++ Assets/Scripts/ScriptableObjects/Shop.cs
@@ -0,0 +1,40 @@
+using Model;
+using UnityEngine;
+
+namespace ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "Shop")]
+    public class Shop : ScriptableObject
+    {
+        [SerializeField] private int _unitCost;
+        [SerializeField] private int _towerCost;
+
+        [SerializeField] private IntVariable _playerMoney;
+        [SerializeField] private IntVariable _enemyMoney;
+        
+        
+        public bool TryBuyUnit(Team team)
+        {
+            var money = team == Team.Player ? _playerMoney : _enemyMoney;
+            if (money.Value >= _unitCost)
+            {
+                money.Value -= _unitCost;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool TryBuyTower(Team team)
+        {
+            var money = team == Team.Player ? _playerMoney : _enemyMoney;
+            if (money.Value >= _towerCost)
+            {
+                money.Value -= _towerCost;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
