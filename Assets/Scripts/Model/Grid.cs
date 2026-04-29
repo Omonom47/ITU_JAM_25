f2b7a00c3c4557c5006a8149b5672843fb6ae657
Maxime Havez <56996234+Omonom47@users.noreply.github.com>
2026-04-29 12:50:51 +0200

    Adds method IsCellOnPath

--- Assets/Scripts/Model/Grid.cs
+++ Assets/Scripts/Model/Grid.cs
@@ -26,6 +26,23 @@ namespace Model
                 ? _checkPoints.ToArray()
                 : _checkPoints?.AsEnumerable().Reverse().ToArray();
 
+        public bool IsCellOnPath(Cell cell)
+        {
+            for (int i = 0; i < _checkPoints.Count-1; i++)
+            {
+                var cur = _checkPoints[i];
+                var next = _checkPoints[i + 1];
+                if (cell.X >= cur.X && cell.X <= next.X && cell.Y == cur.Y && cell.Y == next.Y)
+                {
+                    return true;
+                }if (cell.Y >= cur.Y && cell.Y <= next.Y && cell.X == cur.X && cell.X == next.X)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        
         public Cell GetStartingCheckPoint(Team team)
         {
             return team == Team.Player ? _checkPoints[0] : _checkPoints[^1];
